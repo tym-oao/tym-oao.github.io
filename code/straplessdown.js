747bd6d17f93b7c744eac2d6ca1983714d5b6965
Thomas Yager-Madden <thomas@tym-clio.local>
2016-05-13 14:06:41 -0500

    moar

---
+++ code/straplessdown.js
@@ -0,0 +1,81 @@
+
+;(function(window, document) {
+
+  // Hide body until we're done fiddling with the DOM
+  document.body.style.display = 'none';
+
+  //////////////////////////////////////////////////////////////////////
+  //
+  // Shims for IE < 9
+  //
+
+  document.head = document.getElementsByTagName('head')[0];
+
+  if (!('getElementsByClassName' in document)) {
+    document.getElementsByClassName = function(name) {
+      function getElementsByClassName(node, classname) {
+        var a = [];
+        var re = new RegExp('(^| )'+classname+'( |$)');
+        var els = node.getElementsByTagName("*");
+        for(var i=0,j=els.length; i<j; i++)
+            if(re.test(els[i].className))a.push(els[i]);
+        return a;
+      }
+      return getElementsByClassName(document.body, name);
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////
+  //
+  // Get user elements we need
+  //
+
+  var markdownEl = document.getElementsByTagName('xmp')[0] || document.getElementsByTagName('textarea')[0],
+      titleEl = document.getElementsByTagName('title')[0],
+      scriptEls = document.getElementsByTagName('script'),
+      navbarEl = document.getElementsByClassName('navbar')[0];
+
+
+
+  //////////////////////////////////////////////////////////////////////
+  //
+  // <body> stuff
+  //
+
+  var markdown = markdownEl.textContent || markdownEl.innerText;
+
+  var newNode = document.createElement('div');
+  newNode.className = 'container';
+  newNode.id = 'content';
+  document.body.replaceChild(newNode, markdownEl);
+
+
+  //////////////////////////////////////////////////////////////////////
+  //
+  // Markdown!
+  //
+
+  // Generate Markdown
+  var html = marked(markdown);
+  document.getElementById('content').innerHTML = html;
+
+  // Prettify
+  var codeEls = document.getElementsByTagName('code');
+  for (var i=0, ii=codeEls.length; i<ii; i++) {
+    var codeEl = codeEls[i];
+    var lang = codeEl.className;
+    codeEl.className = 'prettyprint lang-' + lang;
+  }
+  prettyPrint();
+
+  // Style tables
+  var tableEls = document.getElementsByTagName('table');
+  for (var i=0, ii=tableEls.length; i<ii; i++) {
+    var tableEl = tableEls[i];
+    tableEl.className = 'table table-striped table-bordered';
+  }
+
+  // All done - show body
+  document.body.style.display = '';
+
+})(window, document);
